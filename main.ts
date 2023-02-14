a2bbe701575f4cdb9a9f519373ad76dc54754354
Pijarro <124789714+Pijarro@users.noreply.github.com>
2023-02-14 11:28:14 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(200)
     basic.showIcon(IconNames.Skull)
+    basic.pause(200)
+    basic.showIcon(IconNames.No)
+    basic.pause(200)
+    basic.showIcon(IconNames.Angry)
 })
 basic.forever(function () {
 	
